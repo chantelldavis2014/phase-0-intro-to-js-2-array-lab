44c4b35f43fbda88d7df8e6f3400e50de0cf8108
Chantell Davis <chantelldavis2014@yahoo.com>
2022-12-15 14:58:35 -0500

    commit tree 947f4797389d5f54bd515d172edacd1a599f3091

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendCat(name) {
+   cats.push("Ralph") 
+ }
+ 
+ function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+ }
+
+ function destructivelyRemoveLastCat() {
+    cats.pop()
+ }
+
+ function destructivelyRemoveFirstCat() {
+    cats.shift()
+ }
+ function appendCat(name) {
+    let newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+ }
+ function prependCat(name) {
+    let newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+ }
+ function removeLastCat() {
+    let newArray = cats.slice();
+    newArray.pop()
+    return newArray
+ }
+ function removeFirstCat() {
+    let newArray = cats.slice();
+    newArray.shift();
+    return newArray
+ }
+
+
+
+
+
+
+
